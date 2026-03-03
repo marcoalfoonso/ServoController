3698a3a11a8bf8f7931fafb1f25adc77661da3d5
marcoalfoonso <marcoalfoonso.rodcal@gmail.com>
2026-03-03 17:03:08 -0600

    q2 was added

--- servoCont1.js
+++ servoCont1.js
@@ -2,9 +2,14 @@
 document.addEventListener("DOMContentLoaded",()=>{
 
     const q1 = document.getElementById("q1");
+    const q2 = document.getElementById("q2");
 
     q1.oninput = ()=>{
         console.log("q1:",q1.value);
+    }
+
+    q2.oninput = ()=>{
+        console.log("q2: ",q2.value);
     }
 
 });
