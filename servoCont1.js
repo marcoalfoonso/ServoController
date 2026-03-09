0e0f0e52c4e4a227a5d7bbc8918cbb40f95c0730
marcoalfoonso <marcoalfoonso.rodcal@gmail.com>
2026-03-09 13:24:38 -0600

    rectangule was added and the slider was shrinked or moved

--- servoCont1.js
+++ servoCont1.js
@@ -20,6 +20,9 @@ document.addEventListener("DOMContentLoaded",()=>{
 
     const ctx = canvas.getContext("2d");
 
+    ctx.fillStyle = "blue";
+    ctx.fillRect(50, 50, 150, 100);
+
     q1.oninput = ()=>{
         console.log("q1:",q1.value);
 
