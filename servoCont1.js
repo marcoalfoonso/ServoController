5ecf34d26a9d1ac1f72b0502cad8d8eca73a1041
marcoalfoonso <marcoalfoonso.rodcal@gmail.com>
2026-03-10 11:21:29 -0600

    update added in message

--- servoCont1.js
+++ servoCont1.js
@@ -79,7 +79,7 @@ document.addEventListener("DOMContentLoaded",()=>{
             q2.value = value;
         }
 
-        //update(); // redibuja el robot
+        update(); // redibuja el robot
     });
 
     q1.addEventListener("input",()=>{
