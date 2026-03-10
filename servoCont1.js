978a8e5d2937245050a4292b90580e4d5de4b747
marcoalfoonso <marcoalfoonso.rodcal@gmail.com>
2026-03-10 08:59:34 -0600

    subscription was added

--- servoCont1.js
+++ servoCont1.js
@@ -5,6 +5,17 @@ const client = mqtt.connect("wss://e4f0d50b37b04ea79745872566f605ff.s1.eu.hivemq
     clean: true
 });
 
+client.on("connect", () => {
+  console.log("Connecting with Outh");
+
+  client.subscribe("q1", (err)=>{
+    if(!err){
+        console.error("Subscripcion en q1");
+    }else{
+        console.error("Error en subscripcion q1:", err);
+    }
+    });
+});
 
 client.on("error", (err) => {
   console.error("Error:", err);
@@ -76,6 +87,6 @@ document.addEventListener("DOMContentLoaded",()=>{
     }
 
     client.on("message",(topic,message)=>{
-        console.log("Topic: ",topic,"Message: ",message.toString());
+        console.log("Topic: ",topic,"Message: ",message.toString())
     })
 });
